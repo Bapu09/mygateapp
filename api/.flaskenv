d4b144985f50ebb294d564c907e97312dd49fa94
Girija Sankar Dash <girijasankar@google.com>
2020-03-05 13:34:28 +0530

    Login API Returning JSON With Token. No Case Handled

---
+++ api/.flaskenv
@@ -0,0 +1,5 @@
+FLASK_DEBUG=1
+FLASK_ENV=development
+FLASK_APP=run.py
+SECRET_KEY = "TEST"
+UPLOAD_FOLDER = './uploads'
--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,13 +1,38 @@
-from flask import Flask, g, render_template, url_for, redirect, flash, make_response, request, jsonify
-
+from flask import Flask, g, render_template, url_for, redirect, flash, make_response, request, jsonify, json
+import jwt
+from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
 
 app = Flask(__name__)
 
+
+
 @app.route('/login', methods = ['POST'])
 def login():
-    if(request.method == 'POST'):
-        phone = request.args.get('phone')
-        password = request.args.get('pass')
-        
-    else:
-        return jsonify({'status' : False, 'message' : 'Invalid Request', 'code' : 500})
+    phone = request.form['phone']
+    password = request.form['password']
+    
+    token = encode_JWT_token(str(phone))
+    return jsonify({'token' : token, 'status' : True, 'code' : 200})
+    
+    
+    
+def encode_JWT_token(user_email):
+    """
+    We will create JWT Tokens based on the input
+    """
+    
+    try :
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+            'iat': datetime.datetime.utcnow(),
+            'sub': user_email
+        }
+        token = jwt.encode(
+            payload,
+            generate_password_hash('test123'),
+            algorithm='HS256'
+        )
+        return token.decode('utf-8')
+    except Exception as e:
+        return e
